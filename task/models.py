0552c706d82a067ab5b528a553f53566823a4bef
Jagadeesh MV <myqualitywriters1@gmail.com>
2020-07-22 10:32:02 +0400

    final commit

--- task/models.py
+++ task/models.py
@@ -4,10 +4,10 @@ from django.db import models
 class Task(models.Model):
     persons = (
 
-        ('Me', 'me'),
-        ('Njan', 'Njan'),
-        ('Swayam', 'Swayam'),
-        ('Than', 'Than')
+        ('user1', 'User1'),
+        ('user2', 'User2'),
+        ('user3', 'User3'),
+        ('user4', 'User4')
     )
     name = models.CharField(max_length=150)
     created = models.DateTimeField(auto_now_add=True)
